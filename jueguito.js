037352b432e9db28b099f503c30cc80b9177296c
JeremiasB <jeremias.bernardello@outlook.com>
2023-08-08 21:56:30 -0300

    preJuego

--- jueguito.js
+++ jueguito.js
@@ -1,26 +1,33 @@
 //BIENVENIDA
 let preguntonta = prompt("Desea ingresar a mondongo? (si / no)")
+
 while (preguntonta === "si") {
+    //ENTRADA AL JUEGO
     alert("BIENVENIDO A MONDONGO")
+
     // NOMBRE DE JUGADOR
-    let nombreJugador = prompt("Bienvenido aventurero, por favor ingrese su nombre")
+    let nombreJugador = prompt("Como es tu nombre, aventurero?")
     alert("Bienvenido " + nombreJugador + " una gran aventura te espera.")
+
     // ELECCIONES
     let decision = prompt("Te despertaste temprano en un campamento y escuchas ruidos extraños, te levantas a revisar o te quedas en la carpa? (reviso / no reviso)")
 
-    if (decision === "reviso") {
-        let decision1 = prompt("Abriste la carpa para salir y hay un oso robándote la comida, ¿peleas? (si / no)")
-        if (decision1 === "si") {
-            for (let eleccion = 1; eleccion <= 2; eleccion++) {
-                let respuesta = prompt("Te da mucho miedo y piensas en volver a la carpa, ¿lo haces? (si / no)")
-                if (respuesta === "si") {
-                    alert("El oso te escuchó y comenzó a acercarse")
-                    if (eleccion === 2) {
-                        alert("Estás en peligro, ¡el oso te ataca!")
-                    }
+    //PRIMERA ELECCION
+    if(decision === "reviso"){
+        let decision1 = prompt("Un oso esta revisando tu alacena de comida, salis a intentar auyentarlo? (si / no)")
+        
+        if(decision1 === "si"){
+            alert("El oso te mira a los ojos con una mirada parecida al de una bestia salida del infierno")
+
+            for(let i = 1; i <=3; i++){
+                let desafio = prompt("Al darte cuenta de la situacion te da mucho miedo, decidis seguir adelante (1) o volver a la carpa (2)")
+                if(desafio === "1"){
+                    alert("El oso comienza a perseguirte")
+                }else if(desafio === "2"){
+                    alert("El oso se acerca hacia vos")
                 }
             }
         }
     }
-    preguntonta = prompt("¿Desea ingresar a mondongo? (si / no)")
+    preguntonta = prompt("Desea volver a jugar? (si / no)")
 }
